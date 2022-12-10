981e189407173a7bf4cb565365167f3aaa7f9f13
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-09 22:57:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-# Crie um dicionário vazio para representar a agenda de contatos
-
+# Dicionário vazio para representar a agenda de contatos
 agenda = {}
 
+# Função para gerar identificadores inteiros para cada novo contato inserido na agenda
 lista_ids = [0]
-
-
-# Crie uma função para gerar automaticamente identificadores inteiros para cada novo contato inserido na agenda
 def gerar_id_contado(lista_ids):
     id_contato = lista_ids[-1] + 1
     lista_ids.append(id_contato)
     return id_contato
 
-# Crie uma função para ler os dados de um contato
-
-
+# Função para ler os dados de um contato
 def ler_dados_contato():
     nome = input('Nome: ')
     email = input('Email: ')
@@ -22,24 +17,20 @@ def ler_dados_contato():
     dados_contato = (nome, email, telefone)
     return dados_contato
 
-# Crie uma função que insira os dados de um novo contato na agenda de contatos
-
-
+# Função para inserir os dados de um novo contato na agenda de contatos
 def inserir_contato(dados_contato):
     chave_contato = gerar_id_contado(lista_ids)
     # agenda[chave_contato] = dados_contato
     agenda.update({chave_contato: dados_contato})
 
-# Crie uma função que mostre o menu de opções da agenda
-
-
+# Função que mostre o menu de opções da agenda
 def mostrar_menu():
-    print('Opções da Agenda: ')
+    print('Opções da Agenda:')
     print('1 - Inserir contato')
     print('2 - Listar contato(s)')
     print('3 - Sair')
 
-# Crie uma função que mostre todos os contatos
+# Função que lista todos os contatos
 def mostrar_contato(agenda):
     quant_contato = len(agenda)
     if quant_contato == 0:
@@ -49,6 +40,7 @@ def mostrar_contato(agenda):
         for contato in items:
             print(contato)
 
+# Aplicação
 mostrar_menu()
 ler_opcao = input('Digite uma opção: ')
 opcao = int(ler_opcao)
