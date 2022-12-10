3d4df1b9707cd52079ac0e5e16598c0bac7ac7b3
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-09 22:27:56 -0300

    Create ler_dados_contato

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ agenda = {}
 
 lista_ids = [0]
 
+
+# Crie uma função para gerar automaticamente identificadores inteiros para cada novo contato inserido na agenda
 def gerar_id_contado(lista_ids):
     id_contato = lista_ids[-1] + 1
     lista_ids.append(id_contato)
     return id_contato
+
+# Crie uma função para ler os dados de um contato
+def ler_dados_contato():
+    nome = input('Nome: ')
+    email = input('Email: ')
+    telefone = input('Telefone: ')
+
+    dados_contato = (nome, email, telefone)
+    return dados_contato
 
 
