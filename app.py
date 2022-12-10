cc73afa1d7756431ee8993bb2f4dfbd90263f3a0
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-09 22:37:27 -0300

    Create menu

--- app.py
+++ app.py
@@ -26,4 +26,11 @@ def inserir_contato(dados_contato):
     # agenda[chave_contato] = dados_contato    
     agenda.update({chave_contato: dados_contato})
 
+# Crie uma função que função que mostre o menu de opções da agenda
+def menu():
+    print('Opções da Agenda: ')    
+    print('1 - Inserir contato')
+    print('2 - Listar contato(s)')
+    print('3 - Sair')
+
 
