c01afb00de6054b419ad2c5696b75bdd1b37d44c
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-09 22:19:07 -0300

    Create gerar_id_contato

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 # Crie um dicionário vazio para representar a agenda de contatos
 
 agenda = {}
+
+lista_ids = [0]
+
+def gerar_id_contado(lista_ids):
+    id_contato = lista_ids[-1] + 1
+    lista_ids.append(id_contato)
+    return id_contato
+
+
